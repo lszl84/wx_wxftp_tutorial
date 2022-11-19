856f3e86677ecfd2e7aa1520b8e14975c851d7f5
Lukasz <lukasz.sromek@gmail.com>
2022-11-19 13:48:22 +0100

    basic ftp download with cout

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <wx/wx.h>
+#include <wx/protocol/ftp.h>
+
+#include <sstream>
+#include <iostream>
 
 class MyApp : public wxApp
 {
@@ -9,6 +13,9 @@ class MyFrame : public wxFrame
 {
 public:
     MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+
+private:
+    void DownloadData();
 };
 
 wxIMPLEMENT_APP(MyApp);
@@ -25,4 +32,50 @@ MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
 {
     CreateStatusBar();
     SetStatusText("Welcome to wxWidgets!");
+
+    DownloadData();
+}
+
+void MyFrame::DownloadData()
+{
+    wxFTP ftp;
+
+    ftp.Connect("ftp.nasdaqtrader.com");
+    if (ftp.IsConnected())
+    {
+        std::vector<std::string> filenames = {"nasdaqlisted.txt", "otherlisted.txt"};
+
+        ftp.ChDir("/SymbolDirectory");
+
+        for (const auto &fname : filenames)
+        {
+            wxInputStream *in = ftp.GetInputStream(fname);
+            std::stringstream stream;
+
+            if (in)
+            {
+                static constexpr size_t size = 65536;
+                char data[size];
+                do
+                {
+                    in->Read(data, size);
+                    stream << data;
+                } while (in->LastRead() > 0);
+
+                delete in;
+            }
+            else
+            {
+                wxMessageBox("Could not read file.", "Error", wxICON_ERROR);
+            }
+
+            std::cout << stream.str() << std::endl;
+        }
+    }
+    else
+    {
+        wxMessageBox("Could not connect to the server.", "Error", wxICON_ERROR);
+    }
+
+    ftp.Close();
 }
